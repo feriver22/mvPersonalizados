874fc2429445a8deafac5c3404b7204218d7f634
feriver22 <fernandojosesanchez57@gmail.com>
2025-04-08 17:43:53 -0600

    hecho

--- js/carrusel.js
+++ js/carrusel.js
@@ -2,32 +2,32 @@ const carrusel = document.querySelector('.imagenes-carrusel');
 const botonesCarrusel = document.querySelectorAll('.boton-carrusel');
 let indiceActual = 0;
 const imagenes = document.querySelectorAll('.imagen-carrusel');
-let enTransicion = false; // Variable para controlar si está en transición
+let enTransicion = false; 
 
-// Función para mover el carrusel
+
 function moverCarrusel() {
-    if (enTransicion) return; // No permitir movimiento si ya está en transición
-    enTransicion = true; // Marcar que está en transición
+    if (enTransicion) return; 
+    enTransicion = true; 
 
     if (indiceActual >= imagenes.length) {
         indiceActual = 0;
     }
     
-    // Aplicar la transformación al carrusel
+    
     carrusel.style.transform = `translateX(-${indiceActual * 100}%)`;
     actualizarBotones();
 
-    // Después de 0.5s (la duración de la transición), permitir el siguiente movimiento
+    
     setTimeout(() => enTransicion = false, 500);
 }
 
-// Función para actualizar los botones del carrusel
+
 function actualizarBotones() {
     botonesCarrusel.forEach(boton => boton.style.backgroundColor = '#fff');
     botonesCarrusel[indiceActual].style.backgroundColor = '#ffd700';
 }
 
-// Función para mover el carrusel manualmente
+
 botonesCarrusel.forEach(boton => {
     boton.addEventListener('click', () => {
         indiceActual = parseInt(boton.getAttribute('data-index'));
@@ -35,7 +35,7 @@ botonesCarrusel.forEach(boton => {
     });
 });
 
-// Deslizar el carrusel automáticamente cada 5 segundos
+
 setInterval(() => {
     indiceActual++;
     moverCarrusel();
