c6b3e9d03e1451f2ae22e72562c3a2d7388fb1e7
graymonsta <jusanchez708@unicah.edu>
2025-04-08 15:35:49 -0600

    index styles fix

--- js/carrusel.js
+++ js/carrusel.js
@@ -2,14 +2,23 @@ const carrusel = document.querySelector('.imagenes-carrusel');
 const botonesCarrusel = document.querySelectorAll('.boton-carrusel');
 let indiceActual = 0;
 const imagenes = document.querySelectorAll('.imagen-carrusel');
+let enTransicion = false; // Variable para controlar si está en transición
 
 // Función para mover el carrusel
 function moverCarrusel() {
+    if (enTransicion) return; // No permitir movimiento si ya está en transición
+    enTransicion = true; // Marcar que está en transición
+
     if (indiceActual >= imagenes.length) {
         indiceActual = 0;
     }
+    
+    // Aplicar la transformación al carrusel
     carrusel.style.transform = `translateX(-${indiceActual * 100}%)`;
     actualizarBotones();
+
+    // Después de 0.5s (la duración de la transición), permitir el siguiente movimiento
+    setTimeout(() => enTransicion = false, 500);
 }
 
 // Función para actualizar los botones del carrusel
@@ -26,7 +35,7 @@ botonesCarrusel.forEach(boton => {
     });
 });
 
-// Deslizar el carrusel automáticamente cada 3 segundos
+// Deslizar el carrusel automáticamente cada 5 segundos
 setInterval(() => {
     indiceActual++;
     moverCarrusel();
